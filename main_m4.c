2077ec4871bebf6ea896853da8b7e7e76bf08a53
batoolmuharam-lang <batoolmuharam@gmail.com>
2026-05-21 19:40:35 +0300

    Add multiprocessing logic for milestone 4

--- main_m4.c
+++ main_m4.c
@@ -1,35 +1,91 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
+
 #include "Graph.h"
 #include "InputHandler.h"
 #include "Dijkstra.h"
 
 int main() {
+
     Graph *graph = NULL;
     Traveler *travelers = NULL;
     int travelerCount = 0;
 
-    
     if (!loadGraphFromFile("input.txt", &graph, &travelers, &travelerCount)) {
         return 1;
     }
 
-    printf("=== Milestone 4: Core Logic Check ===\n");
-    printf("Successfully loaded %d travelers from file.\n\n", travelerCount);
+    printf("=== Milestone 4: Multi Process System ===\n");
+    printf("Loaded %d travelers successfully.\n\n", travelerCount);
 
     for (int i = 0; i < travelerCount; i++) {
-        printf("[Traveler ID: %d] Calculating shortest path from %d to %d:\n", travelers[i].id, travelers[i].src, travelers[i].dst);
-        
-        dijkstra(graph, travelers[i].src, travelers[i].dst, 
-                 travelers[i].path, &travelers[i].pathLength, &travelers[i].totalDistance);
-        
+
+        printf("[Traveler ID: %d] Calculating shortest path from %d to %d:\n",
+               travelers[i].id,
+               travelers[i].src,
+               travelers[i].dst);
+
+        dijkstra(graph,
+                 travelers[i].src,
+                 travelers[i].dst,
+                 travelers[i].path,
+                 &travelers[i].pathLength,
+                 &travelers[i].totalDistance);
+
         printf("Total Distance: %d\n", travelers[i].totalDistance);
         printf("-----------------------------------------------\n\n");
     }
 
-  
+
+    for (int i = 0; i < travelerCount; i++) {
+
+        pid_t pid = fork();
+
+        if (pid < 0) {
+
+            perror("fork failed");
+            return 1;
+        }
+
+        if (pid == 0) {
+
+            printf("[%d] started\n", getpid());
+
+            pause();
+
+            exit(0);
+
+        } else {
+
+            travelers[i].pid = pid;
+        }
+    }
+
+    printf("Parent created all child processes.\n");
+
+    for (int i = 0; i < travelerCount; i++) {
+
+        sleep(2);
+
+        printf("Parent terminating traveler PID %d\n",
+               travelers[i].pid);
+
+        kill(travelers[i].pid, SIGTERM);
+    }
+
+    for (int i = 0; i < travelerCount; i++) {
+
+        waitpid(travelers[i].pid, NULL, 0);
+    }
+
+    printf("All child processes finished.\n");
+
+
     free(travelers);
     freeGraph(graph);
-    
+
     return 0;
 }
