f698df18fa2dd15f68297d7f06416a76a392d8d0
JanaHirbawi <janahirbawi5@gmail.com>
2026-06-22 21:02:23 +0300

    Fix input file handling in milestone1

--- main.c
+++ main.c
@@ -3,11 +3,16 @@
 #include "InputHandler.h"
 #include "Dijkstra.h"
 
-int main() {
+int main(int argc, char *argv[]) {
     Graph *graph = NULL;
     int source, destination;
 
-    if (!loadGraphFromFile("input.txt", &graph, &source, &destination)) {
+     if (argc != 2) {
+        printf("Usage: %s <input_file>\n", argv[0]);
+        return 1;
+    }
+
+    if (!loadGraphFromFile(argv[1], &graph, &source, &destination)) {
         return 1;
     }
 
